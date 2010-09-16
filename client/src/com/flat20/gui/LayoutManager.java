3400b456581a62a9d3e492774da17d808612de7f
andreas.reuterberg <andreas.reuterberg@3589e2f8-e284-11de-a6a9-d5aeed48c2a6>
2010-09-16 07:57:56 +0000

    Made the layout a bit more flexible. Default values can be specified and then overriden for each controller.

--- client/src/com/flat20/gui/LayoutManager.java
+++ client/src/com/flat20/gui/LayoutManager.java
@@ -3,6 +3,7 @@ package com.flat20.gui;
 import java.io.File;
 
 import java.io.InputStream;
+import java.util.HashMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,7 +15,6 @@ import org.w3c.dom.NodeList;
 
 import android.util.Log;
 
-import com.flat20.fingerplay.midicontrollers.IMidiController;
 import com.flat20.fingerplay.midicontrollers.MidiController;
 import com.flat20.fingerplay.midicontrollers.Parameter;
 import com.flat20.gui.widgets.Pad;
@@ -71,33 +71,118 @@ public class LayoutManager {
 			doc.getDocumentElement().normalize();
 
 			// Figure out if it's new format or not based on the layout width compared to screen width.
-			Element layout = doc.getDocumentElement();
+			Element root = doc.getDocumentElement();
+			Element layout = null;
+			
+			System.out.println("here");
 
-			if (layout.getNodeName().equals("layouts")) {
-				NodeList layouts = doc.getElementsByTagName("layout");
-				int bestDiff = 10000;
+			if (!root.getNodeName().equals("layouts"))
+				return;
 
-				for (int l = 0; l < layouts.getLength(); l++) {
-					if (layouts.item(l).getNodeType() == Node.ELEMENT_NODE) {
-						Element tempLayout = (Element) layouts.item(l);
-						int layoutWidth = getIntegerAttribute(tempLayout, "screenWidth");
-						int diff = Math.abs(width - layoutWidth);
-						if (diff < bestDiff) {
-							bestDiff = diff;
-							layout = tempLayout;
+
+			// Hardcoded defaults.. TODO: Load from the Widgets
+
+			HashMap<String, Parameter[]> defaultParameters = new HashMap<String, Parameter[]>(3);
+			Parameter parameters[];
+
+			// Pad
+			parameters = new Parameter[] {
+					new Parameter(0, 0, 0, "Press", Parameter.TYPE_NOTE, false)
+					};
+			defaultParameters.put("pad", parameters);
+
+			// Slider
+			parameters = new Parameter[] {
+					new Parameter(0, 0, 0, "Press", Parameter.TYPE_CONTROL_CHANGE, true),
+					new Parameter(1, 0, 0, "Vertical", Parameter.TYPE_CONTROL_CHANGE, true)
+					};
+			defaultParameters.put("slider", parameters);
+
+			// XYPad
+			parameters = new Parameter[] {
+					new Parameter(0, 0, 0, "Press", Parameter.TYPE_CONTROL_CHANGE, true),
+					new Parameter(1, 0, 0, "Horizontal", Parameter.TYPE_CONTROL_CHANGE, true),
+					new Parameter(2, 0, 0, "Vertical", Parameter.TYPE_CONTROL_CHANGE, true)
+					};
+			defaultParameters.put("xypad", parameters);
+
+			// Get controller defaults
+			NodeList defaults = doc.getElementsByTagName("defaults");
+			for (int l = 0; l < defaults.getLength(); l++) {
+				if (defaults.item(l).getNodeType() == Node.ELEMENT_NODE) {
+					Element tempDefault = (Element) defaults.item(l);
+
+					// loop through defaults/controllers
+					NodeList controllers = tempDefault.getChildNodes();
+					for (int i = 0; i < controllers.getLength(); i++) {
+						if (controllers.item(i).getNodeType() == Node.ELEMENT_NODE) {
+							Element controller = (Element) controllers.item(i);
+
+							String name = controller.getNodeName();
+							String widgetClass = getStringAttribute(controller, "class", null);
+
+							System.out.println(name + " class = " + widgetClass);
+							/*
+							if (widgetClass != null) {
+								try {
+									Class<?> WidgetClass = Class.forName(widgetClass);
+									Class parameterTypes[] = new Class[] { IMidiController.class };
+									Constructor<?> ct = WidgetClass.getConstructor(parameterTypes);
+									Object argumentList[] = new Object[] { null };
+
+									Widget widget = (Widget) WidgetClass.newInstance();
+
+									//Class WidgetClass = LayoutManager.class.getClassLoader().loadClass(widgetClass);
+									//Widget widget = (Widget) WidgetClass.newInstance();
+									//WidgetClass.getClassLoader().loadClass(widgetClass);
+									//WidgetClass.newInstance();
+									System.out.println(widget);
+									//WidgetClass.cast(obj)
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								}
+							}
+								 */
+
+							
+							parameters = defaultParameters.get(name);
+							if (parameters != null) {
+								System.out.println("Default for " + name);
+								updateParameters(parameters, controller);
+							} else {
+								Log.i("LayoutManager", "Couldn't find hardcoded defaults for " + name);
+							}
 						}
 					}
 				}
 			}
 
-			parseLayout(mainContainer, layout, width, height);
+
+			// Get the correct layout for the current resolution
+			NodeList layouts = doc.getElementsByTagName("layout");
+			int bestDiff = 10000;
+
+			for (int l = 0; l < layouts.getLength(); l++) {
+				if (layouts.item(l).getNodeType() == Node.ELEMENT_NODE) {
+					Element tempLayout = (Element) layouts.item(l);
+					int layoutWidth = getIntegerAttribute(tempLayout, "screenWidth");
+					int diff = Math.abs(width - layoutWidth);
+					if (diff < bestDiff) {
+						bestDiff = diff;
+						layout = tempLayout;
+					}
+				}
+			}
+
+			parseLayout(mainContainer, layout, width, height, defaultParameters);
 
 		} catch (Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
+			//System.out.println(e);
 		}
 	}
 
-	protected static void parseLayout(WidgetContainer mainContainer, Element layout, int androidWidth, int androidHeight) {
+	protected static void parseLayout(WidgetContainer mainContainer, Element layout, int androidWidth, int androidHeight, HashMap<String, Parameter[]> defaultParameters) {
 
 		int numTouchPads = 0;
 		int numSliders = 0;
@@ -115,8 +200,6 @@ public class LayoutManager {
 			
 			if (screens.item(s).getNodeType() == Node.ELEMENT_NODE) {
 
-				Log.i("LayoutManager", "TODO Not reading default values from XML. screen " + s);
-
 				Element screenElement = (Element) screens.item(s);
 
 				int screenX = (int) (getIntegerAttribute(screenElement, "x")*deltaWidth);
@@ -126,6 +209,8 @@ public class LayoutManager {
 				WidgetContainer wc = new WidgetContainer(screenWidth, screenHeight);
 				wc.x = screenX;
 				wc.y = screenY;
+
+				//int autoControllerNumber = 0;
 
 				NodeList widgets = screenElement.getChildNodes();
 				final int wlength =  widgets.getLength();
@@ -140,36 +225,44 @@ public class LayoutManager {
 						int widgetY = (int)(getIntegerAttribute(widgetElement, "y")*deltaHeight);
 						int widgetWidth = (int)(getIntegerAttribute(widgetElement, "width")*deltaWidth);
 						int widgetHeight = (int)(getIntegerAttribute(widgetElement, "height")*deltaHeight);
-						int widgetControllerNumber = getIntegerAttribute(widgetElement, "controllerNumber", IMidiController.CONTROLLER_NUMBER_UNASSIGNED);
+						//int widgetControllerNumber = getIntegerAttribute(widgetElement, "controllerNumber", IMidiController.CONTROLLER_NUMBER_UNASSIGNED);
 
 						MidiController mc = new MidiController();
 						Widget widget = null;
 						if (name.equals("button") || name.equals("pad")) {
 							mc.setName( "Button " + (++numButtons) );
-							Parameter parameters[] = {
-									new Parameter(0, 0, widgetControllerNumber+0, "Press", Parameter.TYPE_NOTE, false)
-									};
+
+							Parameter[] parameters = cloneParameters(defaultParameters.get("pad"));
+							/*
+							for (int i=0; i<parameters.length; i++) {
+								parameters[i].controllerNumber = autoControllerNumber++;
+							}*/
+
 							mc.setParameters( parameters );
 							widget = new Pad(mc);//"Button " + (++numButtons), widgetControllerNumber);
 
 						} else if (name.equals("slider")) {
 							mc.setName( "Slider " + (++numSliders) );
 
-							Parameter parameters[] = {
-									new Parameter(0, 0, widgetControllerNumber+0, "Press", Parameter.TYPE_NOTE, true),
-									new Parameter(1, 0, widgetControllerNumber+1, "Vertical", Parameter.TYPE_CONTROL_CHANGE, true)
-									};
-							mc.setParameters(parameters);
+							Parameter[] parameters = cloneParameters(defaultParameters.get("slider"));
+							/*
+							for (int i=0; i<parameters.length; i++) {
+								parameters[i].controllerNumber = autoControllerNumber++;
+							}*/
+
+							mc.setParameters( parameters );
 							widget = new Slider(mc);//"Slider " + (++numSliders), widgetControllerNumber);
 
 						} else if (name.equals("touchpad") || name.equals("xypad")) {
 							mc.setName( "XY Pad " + (++numTouchPads) );
-							Parameter parameters[] = {
-									new Parameter(0, 0, widgetControllerNumber+0, "Press", Parameter.TYPE_CONTROL_CHANGE, true),
-									new Parameter(1, 0, widgetControllerNumber+1, "Horizontal", Parameter.TYPE_CONTROL_CHANGE, true),
-									new Parameter(2, 0, widgetControllerNumber+2, "Vertical", Parameter.TYPE_CONTROL_CHANGE, true)
-									};
-							mc.setParameters(parameters);
+
+							Parameter[] parameters = cloneParameters(defaultParameters.get("xypad"));
+							/*
+							for (int i=0; i<parameters.length; i++) {
+								parameters[i].controllerNumber = autoControllerNumber++;
+							}*/
+
+							mc.setParameters( parameters );
 							widget = new XYPad(mc);//"XY Pad " + (++numTouchPads), widgetControllerNumber);
 						}
 						else if (name.equals("accelerometer") 
@@ -177,12 +270,14 @@ public class LayoutManager {
 								|| name.equals("magfield")
 								|| name.equals("gyroscope")) {	//3-axis
 							mc.setName( "Sensor " + name + " " + (++numTouchPads) );
-							Parameter parameters[] = {
-									new Parameter(0, 0, widgetControllerNumber+0, "Press", Parameter.TYPE_CONTROL_CHANGE, true),
-									new Parameter(1, 0, widgetControllerNumber+1, "Horizontal", Parameter.TYPE_CONTROL_CHANGE, true),
-									new Parameter(2, 0, widgetControllerNumber+2, "Vertical", Parameter.TYPE_CONTROL_CHANGE, true)
-									};
-							mc.setParameters(parameters);
+
+							Parameter[] parameters = cloneParameters(defaultParameters.get("xypad"));
+							/*
+							for (int i=0; i<parameters.length; i++) {
+								parameters[i].controllerNumber = autoControllerNumber++;
+							}*/
+
+							mc.setParameters( parameters );
 							widget = new SensorXYPad( mc );
 							//widget = new XYPad("Sensor " + name, widgetControllerNumber);
 						}
@@ -192,11 +287,13 @@ public class LayoutManager {
 								|| name.equals("temperature")) {	//single value
 							mc.setName( "Sensor " + name + " " + (++numSliders) );
 
-							Parameter parameters[] = {
-									new Parameter(0, 0, widgetControllerNumber+0, "Press", Parameter.TYPE_NOTE, false),
-									new Parameter(1, 0, widgetControllerNumber+1, "Vertical", Parameter.TYPE_CONTROL_CHANGE, true)
-									};
-							mc.setParameters(parameters);
+							Parameter[] parameters = cloneParameters(defaultParameters.get("slider"));
+							/*
+							for (int i=0; i<parameters.length; i++) {
+								parameters[i].controllerNumber = autoControllerNumber++;
+							}*/
+
+							mc.setParameters( parameters );
 							widget = new SensorSlider(mc);//"Slider " + (++numSliders), widgetControllerNumber);
 						}
 
@@ -207,7 +304,8 @@ public class LayoutManager {
 							wc.addSprite(widget);
 
 							// Load any XML parameters
-							updateParameters(mc, widgetElement);
+							updateParameters(mc.getParameters(), widgetElement);
+
 						}
 					}
 				}
@@ -225,10 +323,23 @@ public class LayoutManager {
 	 * Overrides the default parameters for any controller.
 	 * 
 	 */
-	protected static void updateParameters(IMidiController midiController, Element widgetElement) {
+	protected static void updateParameters(Parameter[] midiControllerParameters, Element widgetElement) {
 
 		//ArrayList<Parameter> parsedParameters = new ArrayList<Parameter>();
 		NodeList parameters = widgetElement.getChildNodes();
+
+		// base values can be set on the widget element itself.
+		int baseChannel = getIntegerAttribute(widgetElement, "channel", -1);
+		int baseControllerNumber = getIntegerAttribute(widgetElement, "controllerNumber", -1);
+
+		for (int id=0; id<midiControllerParameters.length; id++) {
+			Parameter parameter = midiControllerParameters[ id ];
+			if (baseChannel != -1)
+				parameter.channel = baseChannel;
+			if (baseControllerNumber != -1)
+				parameter.controllerNumber = baseControllerNumber + id;
+		}
+
 
 		final int length = parameters.getLength();
 		for (int e = 0; e < length; e++) {
@@ -241,12 +352,18 @@ public class LayoutManager {
 				if (name.equals("parameter")) {
 
 					int parameterId = getIntegerAttribute(parameterElement, "id", -1);
+
+					if (parameterId == -1) {
+						System.out.println("unknown id");
+						continue;
+					}
+
 					int channel = getIntegerAttribute(parameterElement, "channel", -1);
 					int controllerNumber = getIntegerAttribute(parameterElement, "controllerNumber", -1);
 					String parameterName = getStringAttribute(parameterElement, "name", null);
 					String parameterTypeText = getStringAttribute(parameterElement, "type", null);
 
-					Parameter parameter = midiController.getParameters()[parameterId];
+					Parameter parameter = midiControllerParameters[parameterId];
 
 					//if (parameterId != -1)
 						//parameter.i = controllerNumber;
@@ -259,7 +376,7 @@ public class LayoutManager {
 
 					if (controllerNumber != -1)
 						parameter.controllerNumber = controllerNumber;
-					
+
 					if (parameterTypeText != null) {
 						if ("controlChange".equals(parameterTypeText)) {
 							parameter.type = Parameter.TYPE_CONTROL_CHANGE;
@@ -267,8 +384,7 @@ public class LayoutManager {
 							parameter.type = Parameter.TYPE_NOTE;
 						}
 					}
-
-
+					
 				}
 			}
 		}
@@ -303,4 +419,12 @@ public class LayoutManager {
 		return result;
 	}
 
+	// TODO Create a ParameterList class wrapping Parameter[].  
+	private static Parameter[] cloneParameters(Parameter[] parameters) {
+		Parameter[] newParameters = new Parameter[parameters.length];
+		for (int i=0; i<parameters.length; i++) {
+			newParameters[i] = parameters[i].clone();
+		}
+		return newParameters;
+	}
 }
